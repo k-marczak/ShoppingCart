75cfc349c1427643a4d34ae99fc5a010f659b1a6
k-marczak <k_marczak@outlook.com>
2023-02-24 09:59:36 +0100

    Added local DB

--- Infrastracture/DataContext.cs
+++ Infrastracture/DataContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
 
 namespace ShoppingCart.Infrastracture
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
+        {}
+        
+        public DbSet<Product> Products { get; set; }
 
-        }
+        public DbSet<Category> Categories { get; set; }
+        
     }
 }
+
