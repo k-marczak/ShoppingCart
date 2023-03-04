0a38bb768e0c4d3885ff6d81bd6b489c0fed700a
k-marczak <k_marczak@outlook.com>
2023-03-04 19:08:41 +0100

    repair the products code, error

--- Program.cs
+++ Program.cs
@@ -32,6 +32,12 @@ namespace ShoppingCart
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "products",
+                pattern: "/products/{categorySlug?}",
+                    defaults: new {controller = "Products", action = "Index" });
+
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
